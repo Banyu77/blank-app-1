2bf77fdf7c6fe97a14f4c918a58cda7524b0e15d
Banyu77 <banyubiru2868@gmail.com>
2026-05-18 11:53:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("PENENTUAN KELULUSAM")
+st.number_input("Masukkan nilai:")
+    
