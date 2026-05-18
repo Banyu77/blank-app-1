aeb988f33ffeb480585df02fdb0c023d3cba2fc6
Banyu77 <banyubiru2868@gmail.com>
2026-05-18 12:00:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.title("PENENTUAN KELULUSAM")
-st.number_input("Masukkan nilai:"))
-nilai=int(input("Masukkan nilai:")
+st.number_input("Masukkan nilai:")
+nilai=int(input("Masukkan nilai:"))
     if nilai>=55:
         print("STATUS : LULUS")
     else:
